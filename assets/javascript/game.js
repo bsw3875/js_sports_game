ceeee80d104df220e88808f0525c65b965a91710
barbara_workman <susan.workman90@gmail.com>
2020-05-11 18:47:00 -0400

    assets/

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,34 @@
 const teamoneNumshots = document.querySelector("#teamone-numshots")
-teamoneNumshots.addEventListener("click", function () {
-}
-    const teamoneNumgoals = document.querySelector("#teamone-numgoals")
-teamoneNumgoals.addEventListener("click", function () {
-}
-     const teamoneShootButton = document.querySelector("#teamone-shoot-button")
+const teamoneNumgoals = document.querySelector("#teamone-numgoals")
+const teamoneShootButton = document.querySelector("#teamone-shoot-button")
+
+const teamtwoNumshots = document.querySelector("#teamtwo-numshots")
+const teamtwoNumgoals = document.querySelector("#teamtwo-numgoals")
+const teamtwoShootButton = document.querySelector("#teamtwo-shoot-button")
+
+
 teamoneShootButton.addEventListener("click", function () {
-}
-        const teamtwoNumshots = document.querySelector("#teamtwo-numshots")
-teamtwoNumshots.addEventListener("click", function () {
-}
-                const teamtwoNumgoals = document.querySelector("#teamtwo-numgoals")
-teamtwoNumgoals.addEventListener("click", function () {
-}
-                    const teamtwoShootButton = document.querySelector("#teamtwo-shoot-button")
+    console.log("teamoneshoot button clicked");
+    let numbershots = Number(teamoneNumshots.innerHTML) + 1;
+    teamoneNumshots.innerHTML = numbershots;
+})
+
 teamtwoShootButton.addEventListener("click", function () {
+    console.log("teamtwoshoot button clicked");
+    let numbershots = Number(teamtwoNumshots.innerHTML) + 1;
+    teamtwoNumshots.innerHTML = numbershots;
 
 })
 
+const resetButton = document.querySelector("#reset-button")
+
+resetButton.addEventListener("click", function () {
+    console.log("reset-button clicked");
+    let numResets = Number(resetButton.innerHTML) + 1;
+    resetButton.innerHTML = numResets;
+
+
+})
 
 
 
